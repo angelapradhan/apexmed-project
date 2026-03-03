118df3a8f2f018a6078ec8d2b2224a2ad4c348de
angelapradhan <angelapradhan123@gmail.com>
2026-03-03 23:38:36 +0545

    Hospital Controller added

--- backend/controllers/hospital_controller.js
+++ backend/controllers/hospital_controller.js
@@ -2,7 +2,6 @@ const Hospital = require('../models/hospital_model');
 
 const createHospital = async (req, res) => {
     try {
-        // 1. Files haru check garne (Multer middleware le req.files ma rakhchha)
         if (!req.files || !req.files['hospitalLogo'] || !req.files['coverImage']) {
             return res.status(400).json({ success: false, message: "Logo and Cover Image are required!" });
         }
@@ -13,16 +12,14 @@ const createHospital = async (req, res) => {
             departments, feeRange, emergency247, icuAvailable 
         } = req.body;
 
-        // 2. File path haru line
         const hospitalLogo = req.files['hospitalLogo'][0].filename;
         const coverImage = req.files['coverImage'][0].filename;
 
-        // 3. Required fields validation
+        // required fields validation
         if (!hospitalName || !hospitalType || !province || !city) {
             return res.status(400).json({ success: false, message: "Please fill required fields" });
         }
 
-        // 4. Postgres ma save garne (Sequelize)
         const newHospital = await Hospital.create({
             hospitalName, 
             hospitalType, 
@@ -38,8 +35,8 @@ const createHospital = async (req, res) => {
             feeRange, 
             emergency247, 
             icuAvailable,
-            hospitalLogo, // File name save hunchha
-            coverImage    // File name save hunchha
+            hospitalLogo,
+            coverImage   
         });
 
         res.status(201).json({ success: true, message: "Hospital added to Postgres!", hospital: newHospital });
@@ -71,4 +68,25 @@ const getSingleHospital = async (req, res) => {
     }
 };
 
-module.exports = { createHospital, getAllHospitals, getSingleHospital };
+const deleteHospital = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const hospital = await Hospital.findByPk(id);
+        if (!hospital) {
+            return res.status(404).json({ success: false, message: "Hospital not found" });
+        }
+
+        // delete garne
+        await Hospital.destroy({
+            where: { id: id }
+        });
+
+        res.status(200).json({ success: true, message: "Hospital deleted successfully!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server Error: " + error.message });
+    }
+};
+
+module.exports = { createHospital, getAllHospitals, getSingleHospital, deleteHospital};
